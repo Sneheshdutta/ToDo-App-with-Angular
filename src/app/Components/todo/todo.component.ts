14c0ef6a2b0d1635847c24551742d72f17213f67
Snehesh Dutta <sneheshdutta@gmail.com>
2021-07-10 19:12:28 +0530

    todo updated

--- src/app/Components/todo/todo.component.ts
+++ src/app/Components/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/Todo';
 
 @Component({
   selector: 'app-todo',
@@ -7,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  todo!: Todo[];
+
+  constructor() { 
+    this.todo = [
+      {
+        sno: 1,
+        title: "This is title 1",
+        desc: "Give description here",
+        active: true
+      },
+      {
+        sno: 2,
+        title: "This is title 2",
+        desc: "Give description here",
+        active: true
+      },
+      {
+        sno: 3,
+        title: "This is title 3",
+        desc: "Give description here",
+        active: true
+      }
+    ]
+  }
 
   ngOnInit(): void {
   }
